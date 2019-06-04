e5d886e0f79954f82c77dae3cbda085de892d996
Anthony Tran <ajt025@ucsd.edu>
2019-06-04 00:08:07 -0700

    Finished - Building movie list

--- app/src/main/java/com/example/flixster/MovieListActivity.java
+++ app/src/main/java/com/example/flixster/MovieListActivity.java
@@ -1,7 +1,24 @@
 package com.example.flixster;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.flixster.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MovieListActivity extends AppCompatActivity {
 
@@ -10,12 +27,120 @@ public class MovieListActivity extends AppCompatActivity {
     public final static String API_BASE_URL = "https://api.themoviedb.org/3";
     // the parameter name for the API key
     public final static String API_KEY_PARAM = "api_key";
-    // the API key -- TODO move to a secure location
-    public final static String API_KEY = "";
+    // tag for logging
+    public final static String TAG = "MovieListActivity";
+    // list of currently playing movies
+    ArrayList<Movie> movies;
+    // the recycler view
+    RecyclerView rvMovies;
+    // the adapter wired to the recycler view
+    MovieAdapter adapter;
+
+    // instance fields
+    AsyncHttpClient client;
+    // base url for loading images
+    String imageBaseUrl;
+    // poster size to use when fetching images, part of the url
+    String posterSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_movie_list);
+
+        // init. client
+        client = new AsyncHttpClient();
+        // init. list of movies
+        movies = new ArrayList<>();
+        // initialize the adapter -- movies array cannot be reinitialized after this pt
+        adapter = new MovieAdapter(movies);
+
+        // resolve the recycler view and connect a layout manager and the adapter
+        rvMovies = (RecyclerView) findViewById(R.id.rvMovies);
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+        rvMovies.setAdapter(adapter);
+
+        // get config. on app creation
+        getConfiguration();
+    }
+
+    // get the list of currently playing movies from the API
+    private void getNowPlaying() {
+        // create url
+        String url = API_BASE_URL + "/movie/now_playing";
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key)); // API key that is always required
+
+        // execute GET request that expects a JSON object response
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // load the results into movie list
+                try {
+                    JSONArray results = response.getJSONArray("results");
+                    // iterate through result set and create Movie objects
+                    for (int i = 0; i < results.length(); ++i) {
+                        Movie movie = new Movie(results.getJSONObject(i));
+                        movies.add(movie);
+                        // notify adapter that a row was added
+                        adapter.notifyItemInserted(movies.size() - 1);
+                    }
+                    Log.i(TAG, String.format("Loaded %s movies", results.length()));
+                } catch (JSONException e) {
+                    logError("Failed to parse now playing movies", e, true);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                logError("Failed to get data from now playing endpoint", throwable, true);
+            }
+        });
+    }
+
+    // get configuration from API
+    private void getConfiguration() {
+        // create url
+        String url = API_BASE_URL + "/configuration";
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key)); // API key that is always required
+
+        // execute GET request that expects a JSON object response
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONObject images = response.getJSONObject("images");
+                    // get the image base url
+                    imageBaseUrl = images.getString("secure_base_url");
+                    // get the poster size
+                    JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
+                    // use the option at index 3 or w342 as a fallback
+                    posterSize = posterSizeOptions.optString(3, "w342");
+                    Log.i(TAG, String.format("Loaded configuration with imageBaseUrl %s and posterSize %s", imageBaseUrl, posterSize));
+
+                    // get the now playing movie list
+                    getNowPlaying();
+                } catch (JSONException e) {
+                    logError("Failed parsing configuration", e, true);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                logError("Failed getting configuration", throwable, true);
+            }
+        });
+    }
+
+    // handle errors, log and alert user
+    private void logError(String message, Throwable error, boolean alertUser) {
+        // always log the error
+        Log.e(TAG, message, error);
+        // alert the user to avoid silent errors
+        if (alertUser) {
+            // show a long toast w/ error message
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        }
     }
 }
