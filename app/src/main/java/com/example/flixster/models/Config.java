19451da44aeef621022930bceeaf46f2928a6955
Anthony Tran <ajt025@ucsd.edu>
2019-06-21 01:05:03 -0700

    Finished orientation implementation for backdrops

--- app/src/main/java/com/example/flixster/models/Config.java
+++ app/src/main/java/com/example/flixster/models/Config.java
@@ -10,6 +10,8 @@ public class Config {
     String imageBaseUrl;
     // poster size to use when fetching images, part of the url
     String posterSize;
+    // backdrop size to use when fetching backdrop image, part of the url
+    String backdropSize;
 
     public Config(JSONObject object) throws JSONException {
         JSONObject images = object.getJSONObject("images");
@@ -19,6 +21,8 @@ public class Config {
         JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
         // use the option at index 3 or w342 as a fallback
         posterSize = posterSizeOptions.optString(3, "w342");
+        // use the option at index 1 or w780 as a fallback
+        backdropSize = posterSizeOptions.optString(5, "w780");
     }
 
     // helper method for creating urls
@@ -26,11 +30,11 @@ public class Config {
         return String.format("%s%s%s", imageBaseUrl, size, path); // concatenate all 3
     }
 
-    public String getImageBaseUrl() {
-        return imageBaseUrl;
-    }
+    public String getImageBaseUrl() { return imageBaseUrl; }
 
     public String getPosterSize() {
         return posterSize;
     }
+
+    public String getBackdropSize() { return backdropSize; }
 }
