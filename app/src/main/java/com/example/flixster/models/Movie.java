1b9339e43fdf5f0729a422ef77dea1e669db52ee
Anthony Tran <ajt025@ucsd.edu>
2019-06-26 11:45:45 -0700

    Implemented details view

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -2,14 +2,19 @@ package com.example.flixster.models;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
+@Parcel
 public class Movie {
 
     // values
-    private String title;
-    private String overview;
-    private String posterPath; // only path, not full URL
-    private String backdropPath; // only path, not full URL
+    public String title;
+    public String overview;
+    public String posterPath; // only path, not full URL
+    public String backdropPath; // only path, not full URL
+    public Double vote_average;
+
+    public Movie() {}
 
     // initialize from JSON data
     public Movie(JSONObject object) throws JSONException {
@@ -17,6 +22,11 @@ public class Movie {
         overview = object.getString("overview");
         posterPath = object.getString("poster_path");
         backdropPath = object.getString("backdrop_path");
+        vote_average = object.getDouble("vote_average");
+    }
+
+    public Double getVoteAverage() {
+        return vote_average;
     }
 
     public String getTitle() {
